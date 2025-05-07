cd0454b75951e61809ca6b67e252f4c1495dcf8e
AyselS <diospirorf@yandex.ru>
2025-05-07 18:28:01 +0300

    di annotation was added

--- servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,5 +1,6 @@
 package ru.netology.servlet;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.netology.controller.PostController;
 import ru.netology.repository.PostRepository;
 import ru.netology.service.PostService;
@@ -7,7 +8,6 @@ import ru.netology.service.PostService;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class MainServlet extends HttpServlet {
     private PostController controller;
@@ -23,14 +23,13 @@ public class MainServlet extends HttpServlet {
 
     @Override
     public void init() {
-        final var repository = new PostRepository();
-        final var service = new PostService(repository);
-        controller = new PostController(service);
+        final var context = new AnnotationConfigApplicationContext("ru.netology");
+        //final var service = context.getBean(PostService.class);
+        controller = context.getBean(PostController.class);
     }
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) {
-
         // если деплоились в root context, то достаточно этого
         try {
             final var path = req.getRequestURI();
